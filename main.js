16768c73ce0da20e09471c1135346ce0afaebbb8
Goutamx <goutamriwar@gmail.com>
2026-03-13 16:17:51 +0530

    Initial commit - Aurion Music Player

---
+++ main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 800,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      webSecurity: true
+    },
+    backgroundColor: '#0F141E',
+    show: false,
+    icon: path.join(__dirname, 'icon.png')
+  });
+
+  win.loadFile('index.html');
+  
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+
+  // Disable menu bar (optional)
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
